c159a29e84cd3fb809d6e90d12a363cdaacaa4a1
Louise <louisegabriellecc@gmail.com>
2022-04-20 11:46:03 +0100

    handlebars built in helpers (if,each)

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 console.log("hello world! :)");
 
 const express = require("express");
-
+const res = require("express/lib/response");
 const app = express();
+
+// these two lines must be after the lines to get the express application.
+// These lines set configuation for handlers
+app.set("views", __dirname + "/views");
+app.set("view engine", "hbs");
 
 //configure a directory for static files
 app.use(express.static("public")); // we can use this to give the public access to images - they use the url localhost:3000/images/filename.png
@@ -14,23 +19,66 @@ app.use(express.static("public")); // we can use this to give the public access 
 // app.get(".about", (request, response, next) => {})
 
 app.get("/about", (req, res, next) => {
- // console.log(req.url)
+  // console.log(req.url)
   console.log("A request on the ABOUT page was received :)");
-  res.sendFile(__dirname + '/views/about.html'); // Sends the HTML file
+  res.sendFile(__dirname + "/views/about.html"); // Sends the HTML file
 });
 
+//each of these are called a route (app.get)
 app.get("/", (req, res, next) => {
-    // console.log(req.url)
-    console.log("A request on the HOME page was received :)");
-    res.sendFile(__dirname + '/views/home.html');;
+  // console.log(req.url)
+  console.log("A request on the HOME page was received :)");
+  res.sendFile(__dirname + "/views/home.html");
+});
+
+app.get("/contact", (req, res, next) => {
+  res.sendFile(__dirname + "/views/contact.html");
+});
+
+app.get("/product1", (req, res, next) => {
+  res.sendFile(__dirname + "/views/frog-art-1.html");
+});
+
+app.get("/product2", (req, res, next) => {
+  res.sendFile(__dirname + "/views/frog-art-2.html");
+});
+
+app.get("/product3", (req, res, next) => {
+  res.sendFile(__dirname + "/views/frog-art-3.html");
+});
+
+// to render a hbs view res.rend("Nameofthehbsfile", info) **the info you want to pass
+app.get("/master-frog", (req, res, next) => {
+  const data = {
+    title: "Master Frog",
+    price: 1111,
+    imageFile: "frog-art-3.jpg",
+  };
+  res.render("product", data);
+});
+
+app.get("/colourful-frog", (req, res, next) => {
+    const data = {
+      title: "Colourful Frog",
+      price: 555,
+      imageFile: "frog-art-2.webp",
+      sizes: [16,20,24,26],
+    };
+    res.render("product", data);
+  });
+
+  app.get("/watercolour-frog", (req, res, next) => {
+    const data = {
+      title: "Watercolour Frog",
+      price: 222,
+      imageFile: "watercolour-frog.jpg",
+      sizes: [16,20,24,26]
+    };
+    res.render("product", data);
   });
   
 
-app.get("/contact", (req, res, next) => {
-  res.sendFile(__dirname + '/views/contact.html');
-});
 
-app.get("/contact", () => {});
 
 // tell express to start listening and we can define a port. Can have two arguements, first a port, second a function to action if successful
 app.listen(3000, () => {
